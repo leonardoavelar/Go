458715b8e57e1d08ccf43aef44bb20120dd0a24e
Leonardo Avelar <leonardoavelar@terra.com.br>
2024-05-17 02:55:27 -0300

    Inclusao do projeto Data (exemplo de formatacao de datas)

---
+++ src/data/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+    "time"
+)
+
+func main () {
+
+	// dia 			(com zero)				02
+	// dia 			(sem o zero)			2
+	// dia 			da semana (inteiro)		Monday
+	// dia 			da semana abreviado		Mon
+	// mês 			com número (com zero)	01
+	// mês 			com número (sem zero)	1
+	// mês 			(nome inteiro)			January
+	// mês 			(nome abreviado)		Jan
+	// ano 			(inteiro)				2006
+	// ano 			(abreviado)				06
+	// hora			(com zero)				03
+	// hora			(sem zero)				3
+	// hora			(formato 24 horas)		15
+	// minutos 		(com zero)				04
+	// minutos 		(sem zero)				4
+	// segundos 	(com zero)				05
+	// segundos 	(sem zero)				5
+
+	data := time.Now()
+
+    println(data.Format("02/Jan/2006 15:04:05 "))
+
+	println(data.Format("02/01/2006 15:04:05 "))
+	
+	println(data.Format("Monday, 02/01/2006 15:04:05 "))
+	
+}
